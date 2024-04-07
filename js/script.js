68c5a3386fdf95460081abebfefdefa33b46f79a
Maxim <62299292+mjyarush@users.noreply.github.com>
2024-04-07 15:47:53 +0300

    Send modal form to server

--- js/script.js
+++ js/script.js
@@ -1,3 +1,145 @@
 document.addEventListener('DOMContentLoaded', function(){
-    console.log('Document ready');
+
+    // slider
+    let swiper = new Swiper(".swiper", {
+        slidesPerView: 1,
+        loop: true,
+        pagination: {
+            el: "#my-controls .my-swiper-pagination",
+            type: "fraction",
+        },
+        navigation: {
+            nextEl: "#my-controls .my-swiper-button-next",
+            prevEl: "#my-controls .my-swiper-button-prev",
+        },
+
+    });
+    const form = document.querySelector("#modal-lead"),
+        formContainer = document.querySelector('.modal-form-container'),
+        buttonCallForm = document.querySelectorAll('[data-target=modal]'),
+        userMsg = document.querySelector('.user_msg');
+
+    //Event form send
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        trySendForm();
+    });
+
+
+    // Add call modal to each btn
+    buttonCallForm.forEach(btn =>{
+        btn.addEventListener('click', (event)=>{
+            event.preventDefault();
+            formContainer.classList.remove('d-none');
+        });
+    });
+
+    // Close modal if click outside form container
+    formContainer.addEventListener('click', (event) => {
+        const isFormCont = event.target.className === 'modal-form-container';
+
+        if(isFormCont){
+            formContainer.classList.add('d-none');
+        }
+    });
+
+    function trySendForm(){
+        const formData = new FormData(form);
+        let validForm = validateForm(formData);
+
+        hideFormErr();
+        if(validForm.valid){
+            sendData(formData);
+        }else{
+            displayFormErr(validForm.err);
+        }
+
+    }
+    //send data async
+    async function sendData(formData) {
+        try {
+            const response = await fetch("/php/send.php", {
+                method: "POST",
+                body: formData,
+            })
+            const data = await response.json();
+            displayServerMsg(data);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+    function validateForm(formData) {
+        let res = {'valid':false, 'err':{}},
+            regName = new RegExp("^[A-zА-яЁё]+$"),
+            regPhone = new RegExp('^((8|\\+7)[\\- ]?)?(\\(?\\d{3}\\)?[\\- ]?)?[\\d\\- ]{7,10}$'),
+            formField = Object.fromEntries(formData.entries());
+
+        for (const [key, value] of Object.entries(formField)) {
+            switch(key) {
+                case 'user_name':
+                    if(!(regName.test(value))){
+                        res.err.name = 'Для имени допустимы только буквы';
+                    }
+                    break;
+
+                case 'user_phone':
+                    if(!(regPhone.test(value))){
+                        res.err.phone = 'Проверьте правильность номера';
+                    }
+                    break;
+            }
+
+        }
+        if(Object.keys(res.err).length === 0){
+            res.valid = true;
+        }
+
+        return res;
+
+    }
+
+    function displayFormErr(msg) {
+        let nameErr = form.querySelector('#nameErr'),
+            phoneErr = form.querySelector('#phoneErr');
+
+        if(msg.hasOwnProperty('name')) {
+            nameErr.innerHTML = msg.name;
+            nameErr.classList.remove('d-none');
+        }
+        if(msg.hasOwnProperty('phone')){
+            phoneErr.innerHTML = msg.phone;
+            phoneErr.classList.remove('d-none');
+        }
+    }
+    function hideFormErr(){
+        let nameErr = form.querySelector('#nameErr'),
+            phoneErr = form.querySelector('#phoneErr');
+
+            nameErr.classList.add('d-none');
+            phoneErr.classList.add('d-none');
+    }
+    function displayServerMsg(res) {
+        let msg = '';
+
+        if(res.hasOwnProperty('status')){
+            if(res.status == 'send'){
+                msg = '<div class="text-success py-3 text-center fs-4 fw-bold">Сообщение отправлено</div>'
+            }else{
+                msg = res.error;
+            }
+        }else{
+            msg = '<div class="text-danger py-3 text-center fs-4 fw-bold">Что-то пошло не так, свяжитесь с нами по телефону</div>'
+        };
+
+        form.classList.add('d-none');
+        userMsg.innerHTML = msg;
+        userMsg.classList.remove('d-none');
+
+        setTimeout(hideServerMsg, 5000);
+    }
+    function hideServerMsg(){
+        userMsg.classList.add('d-none');
+        form.classList.remove('d-none');
+    }
 });
--- php/send.php
+++ php/send.php
@@ -1,1 +1,56 @@
 <?php
+const TO  = 'rbru-metrika@yandex.ru';// note the comma
+
+//Get field vars
+$formData= [
+    'user_name' => $_POST['user_name'],
+    'user_mail' => $_POST['user_mail'],
+    'user_phone' => $_POST['user_phone'],
+    ];
+
+// subject
+$subject = 'Форма записи';
+
+// message
+$message = '
+<html>
+<head>
+  <title>Birthday Reminders for August</title>
+</head>
+<body>
+  <p>Here are the birthdays upcoming in August!</p>
+  <table>
+    <tr>
+      <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
+    </tr>
+    <tr>
+      <td>Joe</td><td>3rd</td><td>August</td><td>1970</td>
+    </tr>
+    <tr>
+      <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
+    </tr>
+  </table>
+</body>
+</html>
+';
+
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+// Additional headers
+$headers .= 'To: Mary <mary@example.com>, Kelly <kelly@example.com>' . "\r\n";
+$headers .= 'From: Birthday Reminder <birthday@example.com>' . "\r\n";
+$headers .= 'Cc: birthdayarchive@example.com' . "\r\n";
+$headers .= 'Bcc: birthdaycheck@example.com' . "\r\n";
+
+// Mail it
+//mail(TO, $subject, $message, $headers);
+
+$response_msg = [
+    'status' => 'send',
+    'error' => false
+];
+
+
+echo json_encode($response_msg);
